e048b7db1f10c831e3219d2dcfd45589f6db8108
larryzodiac <09kevinsevanmachale@gmail.com>
2018-03-07 16:46:24 +0000

    CA3 Simple Messaging

--- CA3/server.js
+++ CA3/server.js
@@ -3,9 +3,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-
-//keep track of how times clients have clicked the button
-var clickCount = 0;
+var users = {};
 
 app.use(express.static(__dirname + '/public'));
 //redirect / to our index.html file
@@ -13,17 +11,20 @@ app.get('/', function(req, res,next) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-io.on('connection', function(client) {
-	console.log('Client connected...');
-	//when the server receives clicked message, do this
-    client.on('clicked', function(data) {
-    	  clickCount++;
-		  //send a message to ALL connected clients
-		  io.emit('buttonUpdate', clickCount);
-    });
+io.on('connection', function(client){
+  console.log('Client connected...');
+  client.on('disconnect', function(){
+    console.log('Client disconnected...');
+  });
+  client.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
 
 //start our web server and socket.io server listening
 server.listen(3001, function(){
   console.log('listening on *:3001');
 });
+
+
+////////////////////////////////////////////////////
