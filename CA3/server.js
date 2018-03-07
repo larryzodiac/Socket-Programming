df8f69a7d372601f840239b6d9348b9652053537
larryzodiac <09kevinsevanmachale@gmail.com>
2018-03-07 22:54:38 +0000

    CA3 Simple Messaging New

--- CA3/server.js
+++ CA3/server.js
@@ -1,4 +1,6 @@
-const app = require('express')();
+// server.js
+const express = require('express');
+const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const port = 3000;
@@ -7,14 +9,16 @@ server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
-app.get('/', (req, res) => {
+app.use(express.static(__dirname + '/public'));
+//redirect / to our index.html file
+app.get('/', (req, res, next) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
 io.on('connection', (socket) => {
     console.log('user connected');
-    socket.emit('message', { manny: 'hey how are you?'});
-    socket.on('another event', (data) => {
-        console.log(data);
+    socket.on('message', (msg) => {
+        console.log(`message: ${msg}`);
+        io.emit('message', msg);
     })
 })
